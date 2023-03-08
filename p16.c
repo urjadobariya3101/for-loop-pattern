57060d2bd5418e6174d103d0cb567f0063de5814
urjadobariya3101 <115995504+urjadobariya3101@users.noreply.github.com>
2023-03-08 17:10:14 +0530

    Update p16.c

--- p16.c
+++ p16.c
@@ -4,8 +4,8 @@ int main()
 	
 	int i,j;
 	
-	for(i=5 ; i>=1 ; i--){
-		for(j=1 ; j<=i ; j++){
+	for(i=1 ; i<=5 ; i++){
+		for(j=i ; j<=5 ; j++){
 			if(j%2==0){
 				printf("0 ");
 			}
@@ -16,6 +16,7 @@ int main()
 		}
 		printf("\n");
 	}
+	
 	
 	return 0;
 	
